d6abc8e6a8bd2f52b78dbf6b40fd6ee665be158f
Melki Mario Gulo <elhackerz.9je.19@gmail.com>
2024-07-16 13:03:45 +0700

    add recommendation and menu pengembalian

--- app/models.py
+++ app/models.py
@@ -8,16 +8,20 @@ class Student(db.Model):
     class_name = db.Column(db.String(64), nullable=False)
     address = db.Column(db.String(128), nullable=False)
     fingerprint = db.Column(db.String(64), nullable=False)
+    
 
-    loans = db.relationship('Loan', backref='student', lazy=True)
+    loans = db.relationship('Loan', backref='student', lazy=True, cascade='all, delete-orphan')
 
 class Book(db.Model):
     __tablename__ = 'books'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), nullable=False)
-    genre = db.Column(db.String(64), nullable=False)
-
-    loans = db.relationship('Loan', backref='book', lazy=True)
+    title = db.Column(db.String(256), nullable=True)
+    ISBN = db.Column(db.String(256), nullable=False)
+    author = db.Column(db.String(256), nullable=True)
+    year = db.Column(db.String(256), nullable=True)
+    publisher = db.Column(db.String(256), nullable=True)
+    image_url = db.Column(db.String(256), nullable=True)
+    loans = db.relationship('Loan', backref='book', lazy=True, cascade='all, delete-orphan')
 
 class Loan(db.Model):
     __tablename__ = 'loans'
@@ -26,3 +30,4 @@ class Loan(db.Model):
     book_id = db.Column(db.Integer, db.ForeignKey('books.id'), nullable=False)
     loan_date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
     return_date = db.Column(db.Date, nullable=False)
+    is_return = db.Column(db.Boolean, nullable=True, default=False)
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,16 @@ from . import create_app, db
 from .models import Student, Book, Loan
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_wtf.csrf import CSRFProtect
+import pickle
+import pandas
+from datetime import datetime
+import numpy as np
+
+pbr_df = pickle.load(open('PopularBookRecommendation.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+book = pickle.load(open('book.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+
 
 main = Blueprint('main', __name__)
 csrf = CSRFProtect()
@@ -13,7 +23,10 @@ def index():
 
 @main.route('/rekomendasi')
 def rekomendasi_buku():
-    return render_template('rekomendasi_buku.html')
+    list_popular_book = pbr_df['Book-Title'].tolist()
+    books = Book.query.filter(Book.title.in_(list_popular_book)).limit(10).all()
+    
+    return render_template('rekomendasi_buku.html', books=books)
 
 @main.route('/peminjaman')
 def menu_peminjaman():
@@ -21,7 +34,18 @@ def menu_peminjaman():
 
 @main.route('/pengembalian')
 def menu_pengembalian():
-    return render_template('menu_pengembalian.html')
+    loans = db.session.query(
+        Loan.id, Student.name, Student.class_name, Book.ISBN, Book.title, Book.author, Loan.loan_date, Loan.return_date, Loan.is_return
+    ).join(Student).join(Book).all()
+    print(loans)
+    return render_template('menu_pengembalian.html', loans=loans)
+
+@main.route('/pengembalian/<int:id>', methods=['POST'])
+def pengembalian(id):
+    loan = Loan.query.get(id)
+    loan.is_return = True
+    db.session.commit()
+    return redirect(url_for('main.menu_pengembalian'))
 
 @main.route('/registrasi', methods=['GET', 'POST'])
 def registrasi():
@@ -59,3 +83,86 @@ def get_students(fingerprint):
         return {
             'message': 'failed'
         }
+
+
+@main.route('/get_books/<string:ISBN>', methods=['GET'])
+def get_books(ISBN):
+    book = Book.query.filter_by(ISBN=ISBN).first()
+    if book:
+        return {
+            'message': 'success',
+            'id': book.id,
+            'title': book.title,
+            'ISBN': book.ISBN,
+            'author': book.author,
+            'year': book.year,
+            'publisher': book.publisher,
+            'image_url': book.image_url
+        }
+    else:
+        return {
+            'message': 'failed'
+        }
+
+@main.route('/loan', methods=['POST'])
+def loan():
+    data = request.get_json()
+
+    fingerprint_id = data.get('fingerprint_id')
+    # print(fingerprint_id)
+    student = Student.query.filter_by(fingerprint=fingerprint_id).first()
+    # print(student)
+    student_id = student.id
+    book_id = data.get('book_id')
+    loan_date = data.get('loan_date')
+    loan_data = datetime.strptime(loan_date, '%d-%m-%Y')
+    return_date = data.get('return_date')
+    return_data = datetime.strptime(return_date, '%d-%m-%Y')
+
+    new_loan = Loan(student_id=student_id, book_id=book_id, loan_date=loan_data, return_date=return_data)
+    db.session.add(new_loan)
+    db.session.commit()
+
+    return {
+        'message': 'success'
+    }
+
+@main.route('/get_recommendation/<string:fingerprint_id>',methods=["GET"])
+def get_recommendation(fingerprint_id):
+    print(fingerprint_id)
+    student = Student.query.filter_by(fingerprint=fingerprint_id).first()
+    student_id = student.id
+    Loans = db.session.query(Loan.id, Student.name, Student.class_name, Book.ISBN, Book.title, Book.author, Loan.loan_date, Loan.return_date, Loan.is_return).join(Student).join(Book).filter(Loan.student_id == student_id).all()
+
+    book_name = []
+    for i in Loans:
+        book_name.append(i[4])
+
+    data_recommendation = recommendation(book_name[-1])
+
+    book_name_recommendation = []
+    for i in data_recommendation:
+        book_name_recommendation.append(i[0])
+
+    # print(book_name_recommendation)
+    book_recommendation = Book.query.filter(Book.title.in_(book_name_recommendation)).limit(10).all()
+
+
+    return render_template('rekomendasi_buku_cf.html', books=book_recommendation)
+
+
+def recommendation(book_name):
+    # fetching Index
+    index = np.where(np.array(list(pt.index))==book_name)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[0])),reverse=True,key=lambda x:x[1])[1:9]
+    
+    data =[]
+    for i in similar_items:
+        item = []
+        temp_df = book[book['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        
+        data.append(item)
+    return data
